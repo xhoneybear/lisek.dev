7f8655665405cf31f09f486291107b4917620304
Camille Lisek <camille@lisek.dev>
2023-08-03 18:14:13 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+    import '../app.css'
+</script>
+
+<style lang="postcss">
+    :global(html) {
+        min-height: 100vh;
+        background-image: url(bg.webp);
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+    :global(body) {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
+
+<nav class="flex items-center gap-4 p-4 bg-white/50 backdrop-blur-3xl">
+    <img src="logo.png" alt="A pixel art of a red fox sleeping" />
+    <p class="font-bold text-2xl">lisek.dev</p>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
+
+<main class="grow py-16 flex items-center">
+    <slot />
+</main>
+
+<footer class="flex flex-col w-fit m-auto items-center gap-2 py-4 px-16 bg-white/50 backdrop-blur-3xl rounded-2xl">
+    <p>© 2023 lisek.dev | All rights reserved.</p>
+    <p>Made with ❤️ in Svelte</p>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<style>
+    .logo {
+        height: 50vh;
+        image-rendering: pixelated;
+    }
+</style>
+
+<div class="flex flex-col items-center gap-4 w-fit mx-auto pb-4 bg-white/50 backdrop-blur-3xl rounded-2xl overflow-hidden">
+    <div class="flex self-stretch p-2 gap-2 bg-white/50 backdrop-blur-3xl">
+        <div class="w-0 p-2 bg-red-500 rounded-full"></div>
+        <div class="w-0 p-2 bg-yellow-500 rounded-full"></div>
+        <div class="w-0 p-2 bg-green-500 rounded-full"></div>
+        <p class="text-xs font-bold basis-full text-center">Page under construction!</p>
+        <div class="w-16"></div>
+    </div>
+    <img class="logo px-16" src="logo.png" alt="A pixel art of a red fox sleeping" />
+    <p>Please look at this fox in the meantime :)</p>
+    <button class="px-16 py-1 border shadow bg-white hover:bg-blue-600 hover:border-blue-500 active:bg-blue-700 active:border-blue-600 rounded-xl text-sm font-bold">OK</button>
+</div>
