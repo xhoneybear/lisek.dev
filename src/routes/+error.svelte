fc3bdbdb53b468bd89a0c7e67e79965e0c0d0095
Camille Lisek <camille@lisek.dev>
2026-09-05 02:19:02 +0200

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import favicon from '$lib/assets/favicon.svg';
+</script>
+
+<div class="error-page">
+    <img src={favicon} style="height: 100%;" alt="STRAYS logo" />
+    <h1>404</h1>
+    <h2>Page not found</h2>
+</div>
+
+<style>
+    .error-page {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 85vh;
+        text-align: center;
+        grid-column: span 5;
+    }
+
+    h1 {
+        font-size: 6rem;
+    }
+
+    h2 {
+        font-size: 2rem;
+        margin-bottom: 2rem;
+    }
+</style>
